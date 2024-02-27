6a84e9c76508839469a41c2be33e28f9f072b9fe
James Graham <james@hoppipolla.co.uk>
2024-02-27 11:45:06 +0000

    Set script src when script is first executed

--- tranco.js
+++ tranco.js
@@ -1,11 +1,12 @@
+const trancoScriptSrc = new URL(document.currentScript.src);
+
 async function getTrancoUrl(domain) {
   const msg = new TextEncoder().encode(domain);
   const hashBuffer = await crypto.subtle.digest("SHA-256", msg);
   const sha1 = Array.from(new Uint8Array(hashBuffer))
     .map((b) => b.toString(16).padStart(2, "0"))
     .join("");
-  const scriptUrl = new URL(document.currentScript.src);
-  return `${scriptUrl.protocol}://${scriptUrl.hostname}/ranks/domains/${sha1.slice(0,2)}/${sha1.slice(2,4)}/${sha1.slice(4)}`;
+  return `${trancoScriptSrc.protocol}://${trancoScriptSrc.hostname}/ranks/domains/${sha1.slice(0,2)}/${sha1.slice(2,4)}/${sha1.slice(4)}`;
 }
 
 async function rankUrl(url) {
