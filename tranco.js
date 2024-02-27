d6f8836a9c0b4aef25b17275692a503f1d446582
James Graham <james@hoppipolla.co.uk>
2024-02-27 14:10:22 +0000

    Fix URL

--- tranco.js
+++ tranco.js
@@ -6,7 +6,7 @@ async function getTrancoUrl(domain) {
   const sha1 = Array.from(new Uint8Array(hashBuffer))
     .map((b) => b.toString(16).padStart(2, "0"))
     .join("");
-  return `${trancoScriptSrc.protocol}//${trancoScriptSrc.hostname}/ranks/domains/${sha1.slice(0,2)}/${sha1.slice(2,4)}/${sha1.slice(4)}.json`;
+  return `${trancoScriptSrc.protocol}//${trancoScriptSrc.hostname}/tranco-subdomains/ranks/domains/${sha1.slice(0,2)}/${sha1.slice(2,4)}/${sha1.slice(4)}.json`;
 }
 
 async function rankUrl(url) {
